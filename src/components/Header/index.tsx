f97446427551f425990498ecc216f6d2bda4aefb
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-04-08 04:20:36 -0300

    💎 Feature: Added functions to recovery password and authenticated using email/password pattern

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import auth from '@react-native-firebase/auth'
+
 import { ButtonIcon } from '../ButtonIcon';
 
 import { Container, Title } from './styles';
@@ -9,6 +11,16 @@ type Props = {
 }
 
 export function Header({ title, showLogoutButton = false }: Props) {
+
+  async function handleLogout() {
+    try {
+      await auth()
+        .signOut()
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Container showLogoutButton={showLogoutButton}>
       <Title>
@@ -21,7 +33,7 @@ export function Header({ title, showLogoutButton = false }: Props) {
           icon="logout"
           color="alert"
           style={{ marginTop: 20 }}
-          onPress={() => {}}
+          onPress={handleLogout}
         />
       }
     </Container>
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import auth from '@react-native-firebase/auth'
+import { Alert } from 'react-native';
 
 export function SignIn() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function handleAnonymousSignIn() {
+    const { user } = await auth().signInAnonymously()
+    console.log(user)
+  }
+
+  async function handleEmailPasswordRegistering() {
+    try {
+      await auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => console.log('user authenticated successfully'))
+        .catch(error => {
+          switch (error.code) {
+            case 'auth/email-already-in-use':
+              return Alert.alert('Falha ao autenticar-se',
+                'Email já em uso.');
+              break;
+            case 'auth/invalid-email':
+              return Alert.alert('Falha ao autenticar-se',
+                'Email inválido, cheque seu email.');
+              break;
+            case 'auth/weak-password':
+              return Alert.alert('Falha ao autenticar-se',
+                'Senha precisa ter pelo menos 6 dígitos');
+              break;
+              return
+          }
+          console.log(error.code)
+        })
+    } catch (error) {
+      return
+    }
+  }
+
+  async function handleEmailPasswordSignIn() {
+    try {
+      auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(({ user }) => console.log(user.email))
+        .catch(error => {
+          if (error.code === 'auth/user-not-found' ||
+            error.code === 'auth/wrong-password') {
+            return Alert.alert('Falha ao autenticar-se',
+              'Email ou senha incorretos.');
+          }
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function handleRecoveryPassword() {
+    try {
+      await auth()
+        .sendPasswordResetEmail(email)
+        .then(() => {
+          Alert.alert('Redefinição de senha',
+            'Enviamos um e-mail para que você possa redefinir sua senha.')
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+
   return (
     <Container>
       <Title>MyShopping</Title>
@@ -14,18 +85,20 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setEmail}
       />
 
       <Input
         placeholder="senha"
         secureTextEntry
+        onChangeText={setPassword}
       />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title="Entrar" onPress={handleEmailPasswordSignIn} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={handleRecoveryPassword} />
+        <ButtonText title="Criar minha conta" onPress={handleEmailPasswordRegistering} />
       </Account>
     </Container>
   );
